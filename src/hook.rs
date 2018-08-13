8042d2d19ac97a3065b04cad7920b9a1b5de144a
Michael Zhang <failed.down@gmail.com>
2018-08-12 23:34:49 -0700

    hook setup

--- src/hook.rs
+++ src/hook.rs
@@ -1,5 +1,22 @@
+use failure::{err_msg, Error};
 use hyper::{Body, Request, Response};
+use toml::Value;
 
-pub trait Hook: Send + Sync {
-    fn handle(&self, payload: &Request<Body>) -> Option<Response<Body>>;
+pub struct Hook {
+    handler_type: String,
+}
+
+impl Hook {
+    pub fn from(config: &Value) -> Result<Self, Error> {
+        let handler_type = config
+            .get("type")
+            .ok_or(err_msg("Missing field 'type'"))?
+            .as_str()
+            .ok_or(err_msg("Field 'type' is not a string"))?
+            .to_owned();
+        Ok(Hook { handler_type })
+    }
+    pub fn handle(&self, payload: &Request<Body>) -> Option<Response<Body>> {
+        None
+    }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -4,24 +4,27 @@ extern crate hyper;
 extern crate lazy_static;
 extern crate notify;
 extern crate regex;
+extern crate toml;
 extern crate walkdir;
 
 mod handler;
 mod hook;
 
 use std::collections::HashMap;
+use std::fs::File;
+use std::io::Read;
 use std::path::Path;
-use std::path::PathBuf;
-use std::sync::mpsc;
+use std::sync::{mpsc, Mutex};
 use std::thread;
 use std::time::Duration;
 
-use failure::Error;
+use failure::{err_msg, Error};
 use hyper::rt::Future;
 use hyper::service::service_fn_ok;
 use hyper::{Body, Request, Response, Server};
 use notify::{RecommendedWatcher, RecursiveMode, Watcher};
 use regex::Regex;
+use toml::Value;
 use walkdir::WalkDir;
 
 use handler::*;
@@ -31,7 +34,7 @@ lazy_static! {
     static ref URIPATTERN: Regex =
         Regex::new(r"/webhook/(?P<name>[A-Za-z_][A-Za-z0-9_]*)").unwrap();
     static ref HANDLERS: HashMap<String, Box<Handler>> = HashMap::new();
-    static ref HOOKS: HashMap<String, Box<Hook>> = HashMap::new();
+    static ref HOOKS: Mutex<HashMap<String, Hook>> = Mutex::new(HashMap::new());
 }
 
 const NOTFOUND: &str = r#"<html>
@@ -56,7 +59,8 @@ fn service_fn(req: Request<Body>) -> Option<Response<Body>> {
         Some(name) => name.as_str(),
         None => return None,
     };
-    let handler = match HOOKS.get(name) {
+    let hooks = HOOKS.lock().unwrap();
+    let handler = match hooks.get(name) {
         Some(handler) => handler,
         None => return None,
     };
@@ -70,6 +74,52 @@ fn service_fn_wrapper(req: Request<Body>) -> Response<Body> {
     }
 }
 
+fn load_config<P>(root: P)
+where
+    P: AsRef<Path>,
+{
+    println!("Reloading config...");
+    // hold on to the lock while config is being reloaded
+    let mut hooks = HOOKS.lock().unwrap();
+    hooks.clear();
+    let hooks_dir = {
+        let mut p = root.as_ref().to_path_buf();
+        p.push("hooks");
+        p
+    };
+    if hooks_dir.exists() {
+        for entry in WalkDir::new(hooks_dir) {
+            let path = match entry.as_ref().map(|e| e.path()) {
+                Ok(path) => path,
+                _ => continue,
+            };
+            if !path.is_file() {
+                continue;
+            }
+            match (|path: &Path| -> Result<(), Error> {
+                let filename = path
+                    .file_name()
+                    .ok_or(err_msg("what the fuck bro"))?
+                    .to_str()
+                    .ok_or(err_msg("???"))?
+                    .to_owned();
+                let mut file = File::open(path)?;
+                let mut contents = String::new();
+                file.read_to_string(&mut contents)?;
+
+                let config = contents.parse::<Value>()?;
+                let hook = Hook::from(&config)?;
+                hooks.insert(filename, hook);
+                Ok(())
+            })(path)
+            {
+                Ok(_) => (),
+                Err(err) => eprintln!("Failed to read config from {:?}: {}", path, err),
+            }
+        }
+    }
+}
+
 fn watch<P>(root: P) -> notify::Result<()>
 where
     P: AsRef<Path>,
@@ -77,10 +127,13 @@ where
     let (tx, rx) = mpsc::channel();
     let mut watcher: RecommendedWatcher = Watcher::new(tx, Duration::from_secs(1))?;
     println!("Watching {:?}", root.as_ref().to_path_buf());
-    watcher.watch(root, RecursiveMode::Recursive)?;
+    watcher.watch(root.as_ref(), RecursiveMode::Recursive)?;
     loop {
         match rx.recv() {
-            Ok(event) => println!("{:?}", event),
+            Ok(_) => {
+                // for now, naively reload entire config every time
+                load_config(root.as_ref())
+            }
             Err(e) => println!("watch error: {:?}", e),
         }
     }
@@ -90,26 +143,17 @@ pub fn run<P>(root: P) -> Result<(), Error>
 where
     P: AsRef<Path>,
 {
-    let root = root.as_ref().to_path_buf();
-    let handlers_dir = {
-        let mut p = root.clone();
-        p.push("handlers");
-        p
-    };
-    if handlers_dir.exists() {
-        for entry in WalkDir::new(handlers_dir) {
-            let path = entry?.path().to_path_buf();
-            println!("{:?}", path);
-        }
-    }
+    load_config(&root);
 
-    thread::spawn(|| watch(root));
+    let v = root.as_ref().to_path_buf();
+    thread::spawn(|| watch(v));
 
     let addr = ([127, 0, 0, 1], 3000).into();
 
     let server = Server::bind(&addr)
         .serve(|| service_fn_ok(service_fn_wrapper))
         .map_err(|e| eprintln!("server error: {}", e));
+    println!("Listening on {:?}", addr);
     hyper::rt::run(server);
     Ok(())
 }
